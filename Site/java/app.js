9ea8746a34c3122e3e7a741b716963c461cb86f5
HaGotHem <122396503+HaGotHem@users.noreply.github.com>
2023-01-17 21:33:42 +0100

    Add files via upload

--- Site/java/app.js
+++ Site/java/app.js
@@ -1,6 +1,10 @@
-        function fun() {  
-  
-  
-               alert ("This is an alert dialog box");  
-            }  
-                <input type = "button" value = "Click me" onclick = "fun();" />  
+function myFunction() {
+    let text;
+    let person = prompt("Please enter your name:", " Uvuvuevuevue Uvuvuevuevue ossas ");
+    if (person == null || person == "") {
+      alert("Je ne mort pas tu peux me dire ton nom");
+    } else {
+      alert("Bonjour"+" "+person+" "+"Comment allez vous")
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
