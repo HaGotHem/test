b96fd5c27fe5cac804718338e169b239590437eb
HaGotHem <122396503+HaGotHem@users.noreply.github.com>
2023-01-17 20:31:30 +0100

    Add files via upload

---
+++ Site/java/app.js
@@ -0,0 +1,6 @@
+        function fun() {  
+  
+  
+               alert ("This is an alert dialog box");  
+            }  
+                <input type = "button" value = "Click me" onclick = "fun();" />  
